6bab3b1a6250f2da05e75c3b3601f13dc6b37982
Sferra <105938333+FarInHeight@users.noreply.github.com>
2023-01-06 19:10:55 +0100

    adjusted bug for the singular vector signs and modified custom_svd to return just one diagonal matrix

--- custom_svd.m
+++ custom_svd.m
@@ -1,8 +1,30 @@
-function [U, S, S1, V] = custom_svd(A, toll)
+function [U, S, V] = custom_svd(A, toll)
 %CUSTOM_SVD Function to compute the SVD factorization of a matrix
-%   Given a matrix A as input and a tollerance for the stopping criterion
+%   Given a matrix A as input and a tollerance for the stopping criterion,
 %   this function computes the SVD factorization of A.
     [U, S] = singular_vectors(A, toll, true);
     [V, S1] = singular_vectors(A, toll, false);
+
+    minimum = min([length(U), length(V)]);
+
+    if minimum == length(U)
+        S = S1(1:minimum, :);
+        
+        for k = 1:minimum
+            vector = A' * U(:, k);
+            if sign( vector(1) ) ~= sign( V(1, k) )
+                V(:, k) = - V(:, k);
+            end
+        end
+    else
+        S = S(:, 1:minimum);
+
+        for k = 1:minimum
+            vector = A * V(:, k);
+            if sign( vector(1) ) ~= sign( U(1, k) )
+                U(:, k) = - U(:, k);
+            end
+        end
+    end
 end
 
--- singular_vectors.m
+++ singular_vectors.m
@@ -1,13 +1,13 @@
 function [M, H] = singular_vectors(A, toll, left)
 %SINGULAR_VECTORS Function to compute the left or right singular vectors
-%   Given a matrix A as input and a tollerance for the stopping criterion,
-%   this function computes the left or right singular vectors of A and its
+%   Given a matrix A and a tollerance for the stopping criterion, this
+%   function computes the left or right singular vectors of A and its
 %   corrisponding singular values. If left is true then U is calculated, V
 %   otherwise.
     if left
-        [Y, H] = hess(A * A.');
+        [P, H] = hess(A * A.');
     else
-        [Y, H] = hess(A.' * A);
+        [P, H] = hess(A.' * A);
     end
     
     n = length(H);
@@ -15,7 +15,7 @@ function [M, H] = singular_vectors(A, toll, left)
     G = zeros(n - 1, 2);
     G_aux = zeros(2);
 
-    M = Y;
+    M = P;
     err1 = toll + 1;
     err2 = toll + 1;
     
@@ -44,7 +44,6 @@ function [M, H] = singular_vectors(A, toll, left)
             P(k, k+1) = G(k, 2); 
             P(k+1, k) = - G(k, 2);
             P(k+1, k+1) = G(k, 1);
-            P
             M = M * P;
         end
         
@@ -53,10 +52,10 @@ function [M, H] = singular_vectors(A, toll, left)
     end
 
     H = sqrt( diag( diag( H ) ) );
-    if isreal( diag(H) )
+    if all( imag( diag(H) ) < toll )
         H = real(H);
     end
-    if isreal(M)
+    if all( imag( diag(M) ) < toll )
         M = real(M);
     end
 end
