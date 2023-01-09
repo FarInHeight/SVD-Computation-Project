145b2ae93cf5bcea07aeaf1336debbc30e35faff
Sferra <105938333+FarInHeight@users.noreply.github.com>
2023-01-09 21:53:01 +0100

    test and presentation updated

--- test.m
+++ test.m
@@ -1,6 +1,7 @@
 clear;
 
-load './datasets/dataset.mat';
+A = readmatrix('datasets/DATA.csv');
+A = A(2:end, 2:end);
 [U, S, V] = custom_svd(A, 10^-5);
 err = norm(A - U*S*V', "fro");
 plot_singular_values( diag(S) );
