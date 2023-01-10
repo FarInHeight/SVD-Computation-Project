66a8e7e7b21094108d991a1edeaa9f526fef328d
Sferra <105938333+FarInHeight@users.noreply.github.com>
2023-01-10 18:24:38 +0100

    typoscorrected

--- plot_singular_values.m
+++ plot_singular_values.m
@@ -1,15 +1,15 @@
 function plot_singular_values(singular_values)
-%PLOT_SINGULAR_VALUES Plotting singular values magnitudes and energy retained
-%   Given a vector S containing singular values a decreasing order, this
+%PLOT_SINGULAR_VALUES Plotting magnitudes singular values and energy retained
+%   Given a vector S containing singular values in decreasing order, this
 %   function plots three graphs indicating:
-%   - singular values magnitudes vs singualar values indices;
-%   - ritained energy with increasing number of singular values, starting
-%   from higher indeces.
+%   - magnitudes of singular values vs indices of singular values;
+%   - ritained energy as the number of singular values increases, starting
+%   with the highest indeces.
     figure;
     subplot(1, 2, 1);
     bar( singular_values(end:-1:1), 'red');
     xlabel('SINGULAR VALUES FROM SMALLEST TO LARGEST');
-    ylabel('SINGULAR VALUE MAGNITUDES');
+    ylabel('MAGNITUDES OF SINGULAR VALUES');
     subplot(1, 2, 2);
     plot( cumsum( singular_values.^2 ) / sum( singular_values.^2 ), 'LineWidth', 2);
     grid on;
