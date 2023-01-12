2e0c43e9f85888ddcf8ab25649cb160b23438bf2
Sferra <105938333+FarInHeight@users.noreply.github.com>
2023-01-12 13:16:59 +0100

    svd changed to use the same dimensions as the slides

--- custom_svd.m
+++ custom_svd.m
@@ -2,15 +2,64 @@ function [U, S, V] = custom_svd(A, toll)
 %CUSTOM_SVD Function to compute the SVD factorization of a matrix
 %   Given a matrix A as input and a tollerance for the stopping criterion,
 %   this function computes the SVD factorization of A.
+    [U, S] = singular_vectors(A, toll, true);
     [V, S1] = singular_vectors(A, toll, false);
 
-    [New_Diag, s_order] = sort(diag(S1), 'descend');
-    S1 = diag( New_Diag );
-    S1(length(S1), 1) = 0;
-    
-    V = V(s_order,:);
+    dims = size(A);
+    minimum = min(dims);
 
-    S = S1;
-    S1 = diag( 1./ diag( S1 ) );
-    U = A*V*S1;
+    if minimum == dims(1)
+        [U, V, S1] = order_values(S, S1, U, V, 1);
+        
+        for k = 1:minimum
+            B = A';
+            constant = B(1, :) * U(:, k);
+            if sign( constant ) ~= sign( V(1, k) )
+                V(:, k) = - V(:, k);
+            end
+        end
+    else
+        [U, V, S] = order_values(S, S1, U, V, 2);
+
+        for k = 1:minimum
+            constant = A(1, :) * V(:, k);
+            if sign( constant ) ~= sign( U(1, k) )
+                U(:, k) = - U(:, k);
+            end
+        end
+    end
+
+    function [U1, V1, New_S] = order_values(S, S1, U, V, type)
+        [New_Diag, s_order] = sort(diag(S), 'descend');
+        
+        S = diag(New_Diag);
+
+        new_order = 1:dims(1);
+        for h = 1:length(s_order)
+            if h ~= s_order(h)
+                new_order(h) = s_order(h);
+            end
+        end
+    
+        U1 = U(:, new_order);
+
+        [New_Diag, s_order] = sort(diag(S1), 'descend');
+    
+        S1 = diag(New_Diag);
+        
+        new_order = 1:dims(2);
+        for h = 1:length(s_order)
+            if h ~= s_order(h)
+                new_order(h) = s_order(h);
+            end
+        end
+    
+        V1 = V(:, new_order);
+
+        if type == 1
+            New_S = S1(1:minimum, :);
+        else
+            New_S = S(:, 1:minimum);
+        end
+    end
 end
--- test.m
+++ test.m
@@ -3,5 +3,4 @@ clear;
 A = readmatrix('datasets/DATA.csv');
 A = A(2:end, 2:end);
 [U, S, V] = custom_svd(A, 10^-5);
-err = norm(A - U*S*V', "fro");
 plot_singular_values( diag(S) );
